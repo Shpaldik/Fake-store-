165ba5a4c0e726a1dc94bf8917cf9700be5653a1
Shpaldik <shpappy7@gmail.com>
2025-02-27 16:01:09 +0500

    fixed router

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,12 +11,12 @@ defineProps({
 </script>
 
 <template>
-  <router-link :to="`/ProductPage/${id}`">
-    <div
-      class="relative bg-white border border-slate-100 rounded-3xl p-6 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
-    >
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-6 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <router-link :to="`/ProductPage/${id}`">
       <div class="w-full h-60 flex justify-center items-center overflow-hidden rounded-xl">
-        <img :src="imageUrl" alt="Product Image" class="w-full h-full object-cover"/>
+        <img :src="imageUrl" alt="Product Image" class="w-92 h-48 object-cover"/>
       </div>
 
       <p class="mt-2 text-lg font-semibold">{{ title }}</p>
@@ -24,20 +24,20 @@ defineProps({
         <span class="text-slate-400 text-sm">Категория:</span>
         <b class="text-base"> {{ category }}</b>
       </div>
+    </router-link>
 
-      <div class="flex justify-between items-center mt-5">
-        <div class="flex flex-col">
-          <span class="text-slate-400 text-sm">Цена:</span>
-          <b class="text-lg">{{ price }} руб.</b>
-        </div>
-        <img
-          v-if="onClickAdd"
-          @click.stop="onClickAdd"
-          :src="!isAdded ? '/plus.svg' : '/checked.svg'"
-          alt="Add to Cart"
-          class="w-8 h-8 cursor-pointer"
-        />
+    <div class="flex justify-between items-center mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400 text-sm">Цена:</span>
+        <b class="text-lg">{{ price }} руб.</b>
       </div>
+      <img
+        v-if="onClickAdd"
+        @click.stop="onClickAdd"
+        :src="!isAdded ? '/plus.svg' : '/checked.svg'"
+        alt="Add to Cart"
+        class="w-8 h-8 cursor-pointer"
+      />
     </div>
-  </router-link>
+  </div>
 </template>
